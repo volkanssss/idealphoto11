356ad7fe86c948f1be356a20a9439c5b5068c6a5
Volkan-eles <vlkneles@gmail.com>
2026-01-24 19:50:16 +0300

    Increase chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: false
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
   }
 }));
